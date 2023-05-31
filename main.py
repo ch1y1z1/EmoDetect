003ca10b119ccc795da5397a1ff1bf6eb3da829e
chiyizipi <3045059088@qq.com>
2023-05-31 20:00:31 +0800

    main

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import gradio as gr
 imgs = ["pics/" + str(i) + ".jpeg" for i in range(1, 25)] + ["pics/" + str(i) + ".png" for i in range(1, 3)] + ["pics/" + str(i) + ".webp" for i in range(1, 2)]
 
 
-# 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
     gr.Interface(fn=onnx_predict.onnx_predict,
                  inputs=gr.Image(type="pil"),
